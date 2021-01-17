7f58e1d64483e137392f2e9bda94edb34d589f86
R1tschY <r1tschy@posteo.de>
2021-01-17 11:43:02 +0100

    Push also to GitHub CR

--- build.sh
+++ build.sh
@@ -22,7 +22,7 @@ fi
 
 echo "== ⚙️ Building images"
 
-#docker import "target/chroot-i486.tar.gz" "$SDK_BASE_IMAGE:$TARGET_VERSION"
+docker import "target/chroot-i486.tar.gz" "$SDK_BASE_IMAGE:$TARGET_VERSION"
 docker tag "$SDK_BASE_IMAGE:$TARGET_VERSION" "$SDK_BASE_IMAGE:latest"
 
 docker build \
@@ -56,13 +56,3 @@ docker build \
     -t "$SDK_IMAGE:$TARGET_VERSION" \
     -t "$SDK_IMAGE:latest" \
     .
-
-echo "== ⬆️ Pushing to docker hub"
-docker push "$SDK_BASE_IMAGE:$TARGET_VERSION"
-docker push "$SDK_BASE_IMAGE:latest"
-docker push "$SDK_IMAGE:$TARGET_VERSION"
-docker push "$SDK_IMAGE:$TARGET_VERSION-armv7hl"
-docker push "$SDK_IMAGE:$TARGET_VERSION-i486"
-docker push "$SDK_IMAGE:latest"
-docker push "$SDK_IMAGE:armv7hl"
-docker push "$SDK_IMAGE:i486"
--- push.sh
+++ push.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 # dotenv
 export $(egrep -v '^#' env | xargs -d '\n')
 
-echo "== ⬆️ Pushing to docker hub"
+echo "== ⬆️ Pushing to Docker Hub"
 docker push "$SDK_BASE_IMAGE:$TARGET_VERSION"
 docker push "$SDK_BASE_IMAGE:latest"
 docker push "$SDK_IMAGE:$TARGET_VERSION"
@@ -13,3 +13,24 @@ docker push "$SDK_IMAGE:$TARGET_VERSION-i486"
 docker push "$SDK_IMAGE:latest"
 docker push "$SDK_IMAGE:armv7hl"
 docker push "$SDK_IMAGE:i486"
+
+echo "== ⬆️ Pushing to GitHub Container Registry"
+docker tag "$SDK_BASE_IMAGE:$TARGET_VERSION" "$GITHUB_SDK_BASE_IMAGE:$TARGET_VERSION"
+docker tag "$SDK_BASE_IMAGE:latest" "$GITHUB_SDK_BASE_IMAGE:latest"
+docker tag "$SDK_TOOLING_IMAGE:$TARGET_VERSION" "$GITHUB_SDK_TOOLING_IMAGE:$TARGET_VERSION"
+docker tag "$SDK_TOOLING_IMAGE:latest" "$GITHUB_SDK_TOOLING_IMAGE:latest"
+docker tag "$SDK_IMAGE:$TARGET_VERSION-i486" "$GITHUB_SDK_IMAGE:$TARGET_VERSION-i486"
+docker tag "$SDK_IMAGE:i486" "$GITHUB_SDK_IMAGE:i486"
+docker tag "$SDK_IMAGE:$TARGET_VERSION-armv7hl" "$GITHUB_SDK_IMAGE:$TARGET_VERSION-armv7hl"
+docker tag "$SDK_IMAGE:armv7hl" "$GITHUB_SDK_IMAGE:armv7hl"
+docker tag "$SDK_IMAGE:$TARGET_VERSION" "$GITHUB_SDK_IMAGE:$TARGET_VERSION"
+docker tag "$SDK_IMAGE:$TARGET_VERSION" "$GITHUB_SDK_IMAGE:$latest"
+
+docker push "$GITHUB_SDK_BASE_IMAGE:$TARGET_VERSION"
+docker push "$GITHUB_SDK_BASE_IMAGE:latest"
+docker push "$GITHUB_SDK_IMAGE:$TARGET_VERSION"
+docker push "$GITHUB_SDK_IMAGE:$TARGET_VERSION-armv7hl"
+docker push "$GITHUB_SDK_IMAGE:$TARGET_VERSION-i486"
+docker push "$GITHUB_SDK_IMAGE:latest"
+docker push "$GITHUB_SDK_IMAGE:armv7hl"
+docker push "$GITHUB_SDK_IMAGE:i486"
